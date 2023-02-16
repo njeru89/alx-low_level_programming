54e06e6d33f3cd5013d1082920c3aeb6913afac4
catherine njeru <njerucatherine89@gmail.com>
2023-02-16 11:33:01 -0800

    4put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,3 +6,11 @@
  * main - Entry point
  * Description: prints "\"Programming is like building a multilingual puzzle"
  * Return: 0
+ *
+ * Description: This function prints a message to the console using the
+ *              puts() function.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
