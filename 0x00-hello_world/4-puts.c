104190747dbdb12bcdb1e9af33228bde3469b2b2
catherine njeru <njerucatherine89@gmail.com>
2023-02-19 07:24:19 -0800

    4put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- * main entry point
- * Description: prints "\"programming is like building a multilingual puzzle"
- * Return: 0
- * */
-
- * int main(void)
+* main - Entry point
+*
+* Return: Always 0 (Success)
+ **/
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-	}
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
+}
